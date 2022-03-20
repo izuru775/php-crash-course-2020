322ef6782b78a00fb4f337f3b7ebc9f7532ba5bb
izuru775 <izuru775@gmail.com>
2022-03-20 18:06:09 +1100

    Functions completed

--- 08_functions.php
+++ 08_functions.php
@@ -2,10 +2,42 @@
 
 // Function which prints "Hello I am Zura"
 
+// function hello(){
+//     echo "Hello I'm Sodi";
+// }
+// hello();
+
 // Function with argument
+function hello($name){
+    return  "Hello I'm $name";
+}
+echo hello("Sandun").'<br>';
+echo hello("Sodi").'<br>';
 
 // Create sum of two functions
 
-// Create function to sum all numbers using ...$nums
+// function sum($a, $b){
+//     return $a +$b;
+// }
 
+// echo sum(10,5);
+
+// Create function to sum all numbers using ...$nums
+// function sum(...$nums){
+//     $sum = 0;
+//     foreach ($nums as $value) {
+//         $sum += $value;
+//     }
+
+//     return $sum;
+
+// }
+
+// echo sum(1,2,3,4,5,6);
 // Arrow functions
+
+function sum(...$nums){
+   return array_reduce($nums,fn($carry,$n)=>$carry+$n);
+}
+
+echo sum(1,2,3,4,5,6);

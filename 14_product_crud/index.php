8de7edede79537b0b1dbc2455b64a9d887e59988
izuru775 <izuru775@gmail.com>
2022-04-01 20:36:53 +1100

    CRUD operations completed

---
+++ 14_product_crud/index.php
@@ -0,0 +1,66 @@
+<?php
+
+$pdo = new PDO('mysql:host=localhost;port=3306;dbname=product_crud', 'root', '');
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+$statement = $pdo->prepare('SELECT * FROM products ORDER BY create_date DESC');
+$statement->execute();
+$products = $statement->fetchAll(PDO::FETCH_ASSOC);
+// echo '<pre>';
+// echo var_dump($products);
+// echo '</pre>';
+?>
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="app.css">
+    <title>Product CRUD</title>
+</head>
+
+<body>
+    <p>
+        <a href="create.php" class="btn btn-success">Create Product</a>
+    </p>
+    <h1>Product CRUD</h1>
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Image</th>
+                <th scope="col">Title</th>
+                <th scope="col">Price</th>
+                <th scope="col">Create Date</th>
+                <th scope="col">Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($products as $i => $product) : ?>
+                <tr>
+                    <th scope="row"><?php echo $i + 1 ?></th>
+                    <td>
+                        <img class="thumb-image" src="<?php echo $product['image'] ?>" alt="">
+                    </td>
+                    <td><?php echo $product['title'] ?></td>
+                    <td><?php echo $product['price'] ?></td>
+                    <td><?php echo $product['create_date'] ?></td>
+                    <td>
+                        <a href="update.php?id=<?php echo $product['id']?>" class="btn btn-sm btn-outline-primary">Edit</a>
+                        <form style="display:inline-block" action="delete.php" method="post">
+                            <input name="id" type="hidden" value="<?php echo $product['id']?>">
+                            <button type="submit" class="btn btn-sm btn-outline-danger">Delete</button>
+                        </form>
+                    </td>
+                </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
+</body>
+
+</html>
